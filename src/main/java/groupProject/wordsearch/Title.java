3a3b40e2db8fb27176a320622e96fc4a86f81e3e
unknown <18saray@gmail.com>
2016-02-15 22:19:57 -0500

    Added background to GUI

--- src/main/java/groupProject/wordsearch/Title.java
+++ src/main/java/groupProject/wordsearch/Title.java
@@ -4,6 +4,7 @@ import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.RenderingHints;
 
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
