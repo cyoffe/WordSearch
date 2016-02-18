81b59309b27681bb306f9b81c77899e6dc12f5b1
unknown <18saray@gmail.com>
2016-02-18 01:36:35 -0500

    changed button colors

--- src/main/java/groupProject/wordsearch/CategoryGamePanel.java
+++ src/main/java/groupProject/wordsearch/CategoryGamePanel.java
@@ -33,35 +33,35 @@ public class CategoryGamePanel extends JPanel implements ActionListener {
 
 		cat1 = new JButton("HOUSE");
 		cat1.setBorder(BorderFactory.createRaisedBevelBorder());
-		cat1.setBackground(Color.RED);
+		cat1.setBackground(new Color(100, 139, 255));
 
 		cat2 = new JButton("SOUND");
 		cat2.setBorder(BorderFactory.createRaisedBevelBorder());
-		cat2.setBackground(Color.ORANGE);
+		cat2.setBackground(new Color(100, 139, 255));
 		cat3 = new JButton("FRUIT");
 		cat3.setBorder(BorderFactory.createRaisedBevelBorder());
-		cat3.setBackground(Color.YELLOW);
+		cat3.setBackground(new Color(158, 182, 255));
 		cat4 = new JButton("CARS");
 		cat4.setBorder(BorderFactory.createRaisedBevelBorder());
-		cat4.setBackground(Color.GREEN);
+		cat4.setBackground(new Color(158, 182, 255));
 		cat5 = new JButton("NUMBERS");
 		cat5.setBorder(BorderFactory.createRaisedBevelBorder());
-		cat5.setBackground(Color.BLUE);
-		cat6 = new JButton("COMPUTERS");
+		cat5.setBackground(new Color(193, 209, 255));
+		cat6 = new JButton("TECHNOLOGY COMPUTERS");
 		cat6.setBorder(BorderFactory.createRaisedBevelBorder());
-		cat6.setBackground(Color.MAGENTA);
+		cat6.setBackground(new Color(193, 209, 255));
 		cat7 = new JButton("SPORTS");
 		cat7.setBorder(BorderFactory.createRaisedBevelBorder());
-		cat7.setBackground(Color.PINK);
+		cat7.setBackground(new Color(225, 233, 255));
 		cat8 = new JButton("POLITICS");
 		cat8.setBorder(BorderFactory.createRaisedBevelBorder());
-		cat8.setBackground(Color.LIGHT_GRAY);
+		cat8.setBackground(new Color(225, 233, 255));
 		cat9 = new JButton("WEATHER");
 		cat9.setBorder(BorderFactory.createRaisedBevelBorder());
-		cat9.setBackground(Color.GRAY);
+		cat9.setBackground(new Color(246, 248, 255));
 		cat10 = new JButton("MEDICAL");
 		cat10.setBorder(BorderFactory.createRaisedBevelBorder());
-		cat10.setBackground(Color.WHITE);
+		cat10.setBackground(new Color(246, 248, 255));
 
 		cat1.addActionListener(this);
 		cat2.addActionListener(this);
@@ -91,10 +91,12 @@ public class CategoryGamePanel extends JPanel implements ActionListener {
 
 		instructions = new JLabel("Choose a Category", JLabel.CENTER);
 		instructions.setFont(new Font("Arial Black", Font.PLAIN, 50));
+		instructions.setForeground(new Color(0, 63, 255));
 
 		mainMenu = new JButton("MAIN MENU");
 		mainMenu.setBorder(BorderFactory.createRaisedBevelBorder());
-		mainMenu.setBackground(Color.CYAN);
+		mainMenu.setBackground(new Color(0, 63, 255));
+		mainMenu.setForeground(Color.WHITE);
 		mainMenu.addActionListener(new ActionListener() {
 
 			public void actionPerformed(ActionEvent arg0) {
@@ -118,9 +120,8 @@ public class CategoryGamePanel extends JPanel implements ActionListener {
 		String category = source.getText();
 
 		ApiThread api = new ApiThread(category, wordSearchGUI);
-		api.start();		
-		
-		
+		api.start();
+
 	}
 
 	public WordSearch getWordSearch() {
